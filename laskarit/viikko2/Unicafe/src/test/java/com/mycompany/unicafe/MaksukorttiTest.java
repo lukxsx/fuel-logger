68217f8d903329a66e7f6c5e805424fd0b4dc1a4
Lukxsx <luks@luks.moe>
2020-03-16 14:29:51 +0200

    Maksukorttitestit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,38 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void kortinSaldoAlussaOikein() {
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void rahanLataaminenKasvattaaSaldoaOikein() {
+        kortti.lataaRahaa(240);
+        assertEquals("saldo: 2.50", kortti.toString());
+    }
+    
+    @Test
+    public void saldoVaheneeOikeinJosRahaa() {
+        kortti.lataaRahaa(240);
+        kortti.otaRahaa(100);
+        assertEquals("saldo: 1.50", kortti.toString());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosEiRahaa() {
+        kortti.otaRahaa(50);
+        assertEquals(10, kortti.saldo());
+    }
+    
+    @Test
+    public void palauttaaTrueJosRahatRiittaa() {
+        assertEquals(true, kortti.otaRahaa(10));
+    }
+    
+    @Test
+    public void palauttaaFalseJosRahatEiRiita() {
+        assertEquals(false, kortti.otaRahaa(50));
+    }
 }
