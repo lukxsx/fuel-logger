8dce78a8b04768396c86a8c94b5ddead312f26e5
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2020-03-30 20:39:18 +0300

    Harjoitustyöprojektin toimivuuden säätöä

--- fuel-logger/src/main/java/fuellogger/Main.java
+++ fuel-logger/src/main/java/fuellogger/Main.java
@@ -1,9 +1,13 @@
+
 package fuellogger;
+
+import fuellogger.gui.GUI;
 
 public class Main {
 
+
     public static void main(String[] args) {
-
+        GUI.main(args);
     }
-
+    
 }
---
+++ fuel-logger/src/main/java/fuellogger/gui/GUI.java
@@ -0,0 +1,19 @@
+
+package fuellogger.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Fuel logger");
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
