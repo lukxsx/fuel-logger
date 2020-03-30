6b66da850b4bb8cba828ffe5080b965983a1289b
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2020-03-30 22:06:20 +0300

    Updated the working hours, setting up TableView

--- fuel-logger/documentation/working hours.md
+++ fuel-logger/documentation/working hours.md
@@ -1,3 +1,4 @@
 | day | hours | description  |
 | :----:|:-----| :-----|
 | 23.3 | 2    | requirements specification, starting the project |
+| 30.3 | 1    | coding starts, configuring maven project |
--- fuel-logger/src/main/java/fuellogger/gui/GUI.java
+++ fuel-logger/src/main/java/fuellogger/gui/GUI.java
@@ -1,22 +1,47 @@
-
 package fuellogger.gui;
 
 import fuellogger.dao.Database;
+
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Database db = new Database();
         
+        // window settings
         primaryStage.setTitle("Fuel logger");
+
+        // car selection view
+        VBox carSelectLayout = new VBox();
+
+        Label carSelectInfo = new Label("Select a car to use");
+        carSelectLayout.getChildren().add(carSelectInfo);
+
+        TableView carSelect = new TableView();
+        TableColumn csNameColumn = new TableColumn("Car");
+        TableColumn csOdometerColumn = new TableColumn("Odometer");
+        TableColumn csAvgColumn = new TableColumn("Avg. consumption");
+        carSelect.getColumns().addAll(csNameColumn, csOdometerColumn, csAvgColumn);
+        
+        carSelectLayout.getChildren().add(carSelect);
+        
+        Scene carSelectScene = new Scene(carSelectLayout);
+
+        primaryStage.setScene(carSelectScene);
         primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
