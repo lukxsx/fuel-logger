39bca3c8c330aba25343df0a339fc7983ff404ec
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2020-03-30 21:00:29 +0300

    Added a class to control the database

--- fuel-logger/src/main/java/fuellogger/Main.java
+++ fuel-logger/src/main/java/fuellogger/Main.java
@@ -5,7 +5,6 @@ import fuellogger.gui.GUI;
 
 public class Main {
 
-
     public static void main(String[] args) {
         GUI.main(args);
     }
--- fuel-logger/src/main/java/fuellogger/dao/Database.java
+++ fuel-logger/src/main/java/fuellogger/dao/Database.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fuellogger.dao;
 
-/**
- *
- * @author luks
- */
+import java.sql.*;
+
 public class Database {
-    
+
+    Connection db;
+
+    public Database() throws SQLException {
+        db = DriverManager.getConnection("jdbc:sqlite:database.db");
+    }
+
 }
