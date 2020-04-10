d51f9845464ae914e68f8a7cea86614d43a7bbc0
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2020-04-10 12:51:18 +0300

    updated gitignore

--- fuel-logger/src/test/java/fuellogger/domain/RefuelingTest.java
+++ fuel-logger/src/test/java/fuellogger/domain/RefuelingTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fuellogger.domain;
 
 import java.time.LocalDate;
@@ -14,10 +9,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author luks
- */
 public class RefuelingTest {
     
     public RefuelingTest() {
