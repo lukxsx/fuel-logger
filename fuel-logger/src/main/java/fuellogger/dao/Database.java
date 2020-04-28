f8dd4ccd4d3071184f5472afde5f066bcb1aced1
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2020-04-28 19:49:10 +0300

    use try-catchs on database class

--- fuel-logger/src/main/java/fuellogger/dao/Database.java
+++ fuel-logger/src/main/java/fuellogger/dao/Database.java
@@ -38,22 +38,22 @@ public class Database {
         return true;
     }
 
-    public ArrayList<Car> getCars() throws SQLException {
+    public ArrayList<Car> getCars() {
         ArrayList<Car> cars = new ArrayList<>();
         try {
-        PreparedStatement carQuery = db.prepareStatement("SELECT * FROM Car");
-        ResultSet carResults = carQuery.executeQuery();
-        while (carResults.next()) {
-            cars.add(new Car(carResults.getString("name"),
-                    carResults.getInt("fuel_capacity")));
-        }
+            PreparedStatement carQuery = db.prepareStatement("SELECT * FROM Car");
+            ResultSet carResults = carQuery.executeQuery();
+            while (carResults.next()) {
+                cars.add(new Car(carResults.getString("name"),
+                        carResults.getInt("fuel_capacity")));
+            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
         return cars;
     }
 
-    public boolean addRefill(Refueling refueling) throws SQLException {
+    public boolean addRefill(Refueling refueling) {
         int carid = getCarId(refueling.car);
         if (carid == 0) {
             return false;
@@ -75,67 +75,82 @@ public class Database {
         return true;
     }
 
-    public int getCarId(Car car) throws SQLException {
-        PreparedStatement carIdQuery = db.prepareStatement("SELECT id FROM Car WHERE name=?");
-        carIdQuery.setString(1, car.getName());
-        ResultSet idQueryResults = carIdQuery.executeQuery();
-        if (idQueryResults.next()) {
-            return idQueryResults.getInt("id");
-        } else {
-            return 0;
+    public int getCarId(Car car) {
+        try {
+            PreparedStatement carIdQuery = db.prepareStatement("SELECT id FROM Car WHERE name=?");
+            carIdQuery.setString(1, car.getName());
+            ResultSet idQueryResults = carIdQuery.executeQuery();
+            if (idQueryResults.next()) {
+                return idQueryResults.getInt("id");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
-
+        return 0;
     }
 
-    public Car getCar(int id) throws SQLException {
-        PreparedStatement carQuery = db.prepareStatement("SELECT * FROM Car WHERE id=?");
-        carQuery.setInt(1, id);
-        ResultSet carQresults = carQuery.executeQuery();
-        if (carQresults.next()) {
-            return new Car(carQresults.getString("name"), carQresults.getInt("fuel_capacity"));
-        } else {
-            return null;
+    public Car getCar(int id) {
+        try {
+            PreparedStatement carQuery = db.prepareStatement("SELECT * FROM Car WHERE id=?");
+            carQuery.setInt(1, id);
+            ResultSet carQresults = carQuery.executeQuery();
+            if (carQresults.next()) {
+                return new Car(carQresults.getString("name"), carQresults.getInt("fuel_capacity"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
-
+        return null;
     }
 
-    public ArrayList<Refueling> getRefuelings() throws SQLException {
+    public ArrayList<Refueling> getRefuelings() {
         ArrayList<Refueling> refuelings = new ArrayList<>();
-        PreparedStatement refQuery = db.prepareStatement("SELECT * FROM Refueling");
-        ResultSet refResults = refQuery.executeQuery();
-        while (refResults.next()) {
-            LocalDate d = LocalDate.of(refResults.getInt("year"),
-                    refResults.getInt("month"), refResults.getInt("day"));
-            refuelings.add(new Refueling(getCar(refResults.getInt("car_id")),
-                    refResults.getInt("odometer"),
-                    refResults.getDouble("volume"), 
-                    refResults.getDouble("price"), d));
+        try {
+            PreparedStatement refQuery = db.prepareStatement("SELECT * FROM Refueling");
+            ResultSet refResults = refQuery.executeQuery();
+            while (refResults.next()) {
+                LocalDate d = LocalDate.of(refResults.getInt("year"),
+                        refResults.getInt("month"), refResults.getInt("day"));
+                refuelings.add(new Refueling(getCar(refResults.getInt("car_id")),
+                        refResults.getInt("odometer"),
+                        refResults.getDouble("volume"),
+                        refResults.getDouble("price"), d));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
         return refuelings;
     }
 
-    public ArrayList<Refueling> getRefuelings(Car car) throws SQLException {
+    public ArrayList<Refueling> getRefuelings(Car car) {
         ArrayList<Refueling> refuelings = new ArrayList<>();
-        PreparedStatement refQuery = db.prepareStatement("SELECT * FROM Refueling WHERE car_id=?");
-        refQuery.setInt(1, getCarId(car));
-        ResultSet refResults = refQuery.executeQuery();
-        while (refResults.next()) {
-            LocalDate d = LocalDate.of(refResults.getInt("year"),
-                    refResults.getInt("month"), refResults.getInt("day"));
-            Refueling r = new Refueling(car, refResults.getInt("odometer"), 
-                    refResults.getDouble("volume"), refResults.getDouble("price"), d);
-            refuelings.add(r);
+        try {
+            PreparedStatement refQuery = db.prepareStatement("SELECT * FROM Refueling WHERE car_id=?");
+            refQuery.setInt(1, getCarId(car));
+            ResultSet refResults = refQuery.executeQuery();
+            while (refResults.next()) {
+                LocalDate d = LocalDate.of(refResults.getInt("year"),
+                        refResults.getInt("month"), refResults.getInt("day"));
+                Refueling r = new Refueling(car, refResults.getInt("odometer"),
+                        refResults.getDouble("volume"), refResults.getDouble("price"), d);
+                refuelings.add(r);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
         }
 
         return refuelings;
     }
 
-    public void clear() throws SQLException {
+    public void clear() {
         // used for tests
-        Statement s = db.createStatement();
-        s.execute("DELETE FROM Car");
-        s.execute("DELETE FROM Refueling");
-
+        try {
+            Statement s = db.createStatement();
+            s.execute("DELETE FROM Car");
+            s.execute("DELETE FROM Refueling");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
