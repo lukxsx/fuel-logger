7b5955b8d60a268f803dc3a000edecaf7796f6d3
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2020-05-08 16:47:18 +0300

    alert dialogs

--- fuel-logger/src/main/java/fuellogger/gui/GUI.java
+++ fuel-logger/src/main/java/fuellogger/gui/GUI.java
@@ -8,7 +8,6 @@ import java.text.DateFormatSymbols;
 import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.function.UnaryOperator;
 import java.util.regex.Pattern;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -22,6 +21,8 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.Chart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
@@ -33,7 +34,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TableView.TableViewSelectionModel;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextFormatter;
-import javafx.scene.control.TextFormatter.Change;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -43,7 +43,7 @@ import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
-    
+
     private Logic l;
     private Scene refuelingsScene;
     private Scene carSelectScene;
@@ -55,35 +55,34 @@ public class GUI extends Application {
         Pattern decimalPattern = Pattern.compile("-?\\d*(\\.\\d{0,3})?");
         return new TextFormatter<>(c -> (decimalPattern.matcher(c.getControlNewText()).matches()) ? c : null);
     }
-    
+
     private TextFormatter<Double> integerFormatter() {
         Pattern integerPattern = Pattern.compile("[0-9]*");
         return new TextFormatter<>(c -> (integerPattern.matcher(c.getControlNewText()).matches()) ? c : null);
     }
-    
-    
+
     @Override
     public void start(Stage primaryStage) {
-        
+
         l = new Logic("database.db");
         this.currentCar = null;
         // window settings
         primaryStage.setHeight(1024);
         primaryStage.setWidth(800);
         primaryStage.setTitle("Fuel logger");
-        
+
         carSelectScene = carSScene(primaryStage);
         refuelingsScene = null;
         graphScene = null;
-        
+
         primaryStage.setScene(carSelectScene);
         primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
-    
+
     public Scene carSScene(Stage primaryStage) {
         /*
         *
@@ -92,7 +91,7 @@ public class GUI extends Application {
          */
         VBox carSelectLayout = new VBox();
         carSelectLayout.setPadding(new Insets(10));
-        
+
         Label carSelectInfo = new Label("Select a car to use");
         carSelectInfo.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 18));
         carSelectLayout.getChildren().add(carSelectInfo);
@@ -100,7 +99,7 @@ public class GUI extends Application {
         // tableview for car selection
         TableView carSelect = new TableView();
         carSelect.setEditable(true);
-        
+
         TableColumn<String, Car> csNameColumn = new TableColumn("Car");
         csNameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
         carSelect.getColumns().addAll(csNameColumn);
@@ -124,7 +123,7 @@ public class GUI extends Application {
             if (!(selectedCar.size() == 0)) {
                 currentCar = selectedCar.get(0);
                 refuelingsScene = refuelScene(primaryStage);
-                
+
                 primaryStage.setScene(refuelingsScene);
             }
         });
@@ -135,22 +134,22 @@ public class GUI extends Application {
         TextField csFuelCField = new TextField();
         csFuelCField.setPromptText("Fuel capacity");
         Button csAddButton = new Button("Add");
-        
+
         csFuelCField.setTextFormatter(integerFormatter());
-        
+
         csAddButton.setOnAction((ActionEvent e) -> {
             if (!(csNameField.getText().isBlank()) && !(csFuelCField.getText().isBlank())) {
-                
+
                 Car c = new Car(csNameField.getText(), Integer.valueOf(csFuelCField.getText()));
                 if (l.addCar(c)) {
                     carData.add(c);
-                    
+
                 }
             }
             csNameField.clear();
             csFuelCField.clear();
         });
-        
+
         Label csAddInfo = new Label("Add a new car");
         csAddInfo.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 18));
         HBox csAddLayout = new HBox();
@@ -165,39 +164,41 @@ public class GUI extends Application {
         carSelectLayout.getChildren().add(separator);
         carSelectLayout.getChildren().add(csAddInfo);
         carSelectLayout.getChildren().add(csAddLayout);
-        
+
         Scene css = new Scene(carSelectLayout);
         return css;
     }
-    
+
     public Scene refuelScene(Stage primaryStage) {
         /*
         *
         *   Refuelings view
         *
          */
-        
+
         VBox refuelLayout = new VBox();
         refuelLayout.setPadding(new Insets(10));
-        
+
         HBox refuelTopLayout = new HBox();
         refuelTopLayout.setSpacing(20);
-        
+        Label heading = new Label("Refuelings");
+        heading.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 20));
+        refuelTopLayout.getChildren().add(heading);
         Label rfCarLabel = new Label();
         rfCarLabel.setText("Selected car: " + currentCar.getName());
-        rfCarLabel.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 18));
+        rfCarLabel.setFont(Font.font("verdana", FontPosture.REGULAR, 14));
         Button back = new Button("Car selection");
-        
+
         refuelTopLayout.getChildren().add(rfCarLabel);
         Separator s1 = new Separator(Orientation.VERTICAL);
-             refuelTopLayout.getChildren().add(s1);
+        refuelTopLayout.getChildren().add(s1);
         refuelTopLayout.getChildren().add(back);
         Button rfGraphsButton = new Button("Charts");
         refuelTopLayout.getChildren().add(rfGraphsButton);
-        
+
         TableView refills = new TableView();
         refills.setEditable(true);
-        
+
         TableColumn<Integer, Refueling> rfOdometerColumn = new TableColumn("Odometer");
         rfOdometerColumn.setCellValueFactory(new PropertyValueFactory<>("odometer"));
         TableColumn<Double, Refueling> rfVolumeColumn = new TableColumn("Volume");
@@ -206,7 +207,7 @@ public class GUI extends Application {
         rfPriceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
         TableColumn<LocalDate, Refueling> rfDateColumn = new TableColumn("Date");
         rfDateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
-        
+
         refills.getColumns().addAll(rfOdometerColumn, rfVolumeColumn, rfPriceColumn, rfDateColumn);
 
         // date for table
@@ -235,9 +236,7 @@ public class GUI extends Application {
         Button rfAddButton = new Button("Add");
         rfAddLayout.setSpacing(10);
         rfAddLayout.getChildren().addAll(odField, volField, priceField, dateField, rfAddButton);
-        
-        
-        
+
         refuelLayout.getChildren().add(refuelTopLayout);
         refuelLayout.getChildren().add(refills);
         Separator s2 = new Separator(Orientation.HORIZONTAL);
@@ -245,62 +244,72 @@ public class GUI extends Application {
         refuelLayout.getChildren().add(s2);
         refuelLayout.getChildren().add(carStats(currentCar));
         refuelLayout.setSpacing(10);
-        
+
         rfAddButton.setOnAction((ActionEvent e) -> {
             if (!odField.getText().isBlank() && !volField.getText().isBlank()
-                    && !priceField.getText().isBlank() && dateField.getValue() != null) {
-                
-                int odo = Integer.valueOf(odField.getText());
-                double vol = Double.valueOf(volField.getText());
-                double pr = Double.valueOf(priceField.getText());
-                LocalDate date = dateField.getValue();
-                
-                Refueling r = new Refueling(currentCar, odo, vol, pr, date);
-                l.addRefueling(r);
-                refuelingData.add(r);
+                    && !priceField.getText().isBlank()) {
+                if (dateField.getValue() != null) {
+                    int odo = Integer.valueOf(odField.getText());
+                    double vol = Double.valueOf(volField.getText());
+                    double pr = Double.valueOf(priceField.getText());
+                    LocalDate date = dateField.getValue();
+
+                    Refueling r = new Refueling(currentCar, odo, vol, pr, date);
+                    l.addRefueling(r);
+                    refuelingData.add(r);
+                } else {
+                    Alert error = new Alert(AlertType.ERROR);
+                    error.setTitle("Error");
+                    error.setContentText("Invalid date");
+                    error.showAndWait();
+                }
+
+            } else {
+                Alert error = new Alert(AlertType.ERROR);
+                error.setTitle("Error");
+                error.setContentText("Invalid or blank input");
+                error.showAndWait();
             }
-            odField.clear();
-            volField.clear();
-            dateField.getEditor().clear();
+
             this.refuelingsScene = refuelScene(primaryStage);
             primaryStage.setScene(refuelingsScene);
-            
+
         });
-        
+
         rfGraphsButton.setOnAction((ActionEvent e) -> {
             graphScene = graphsScene(primaryStage);
             primaryStage.setScene(graphScene);
         });
-        
+
         back.setOnAction((ActionEvent e) -> {
             currentCar = null;
             primaryStage.setScene(carSelectScene);
         });
-        
+
         Scene rfs = new Scene(refuelLayout);
         return rfs;
     }
-    
+
     public Scene graphsScene(Stage primaryStage) {
         /*
         *
         *   Graphs view
         *
          */
-        
+
         HBox graphsTop = new HBox();
         graphsTop.setSpacing(15);
         graphsTop.setPadding(new Insets(5));
-        
+
         ChoiceBox cb = new ChoiceBox();
         Label cbLabel = new Label("Select graph:");
         cb.getItems().add("Consumption by month");
         cb.getItems().add("Cost by month");
         cb.getItems().add("Kilometres by month");
         cb.setValue("Consumption by month");
-        
+
         Button back = new Button("Back");
-        
+
         LocalDate today = LocalDate.now();
         ChoiceBox yearselect = new ChoiceBox();
         for (int i = 0; i < 10; i++) {
@@ -308,30 +317,30 @@ public class GUI extends Application {
         }
         yearselect.setValue(today.getYear());
         Button yearSelectButton = new Button("Select");
-        
+
         VBox grLayout = new VBox();
         grLayout.setPadding(new Insets(10));
         graphsTop.getChildren().addAll(back, cbLabel, cb, yearselect, yearSelectButton);
-        
+
         back.setOnAction((ActionEvent e) -> {
             primaryStage.setScene(refuelingsScene);
         });
-        
+
         grLayout.getChildren().add(graphsTop);
         Separator s2 = new Separator(Orientation.HORIZONTAL);
         grLayout.getChildren().add(s2);
-        
+
         VBox graphPane = new VBox();
-        
+
         chart = monthChart((int) yearselect.getValue());
         graphPane.getChildren().add(chart);
         grLayout.getChildren().add(graphPane);
-        
+
         VBox grBottomLayout = carStats(currentCar);
         Separator s = new Separator(Orientation.HORIZONTAL);
         grLayout.getChildren().add(s);
         grLayout.getChildren().add(grBottomLayout);
-        
+
         yearSelectButton.setOnAction((ActionEvent e) -> {
             String value = (String) cb.getValue();
             switch (value) {
@@ -345,113 +354,113 @@ public class GUI extends Application {
                     chart = kmsChart((int) yearselect.getValue());
                     break;
             }
-            
+
             graphPane.getChildren().clear();
             graphPane.getChildren().add(chart);
         });
-        
+
         Scene grs = new Scene(grLayout);
         return grs;
     }
-    
+
     private Chart monthChart(int year) {
-        
+
         CategoryAxis consXAxis = new CategoryAxis();
         NumberAxis consYAxis = new NumberAxis();
         consXAxis.setLabel("Month");
         consYAxis.setLabel("l/100 km");
-        
+
         BarChart<String, Number> consChart = new BarChart<>(consXAxis, consYAxis);
         consChart.setTitle("Fuel consumption by month");
-        
+
         XYChart.Series consData = new XYChart.Series();
-        
+
         for (int i = 1; i <= 12; i++) {
             consData.getData().add(new XYChart.Data(getMonthName(i), l.monthAvg(currentCar, i, year)));
         }
-        
+
         consChart.getData().add(consData);
         consChart.setLegendVisible(false);
         return consChart;
     }
-    
+
     private Chart costChart(int year) {
         CategoryAxis costXAxis = new CategoryAxis();
         NumberAxis costYAxis = new NumberAxis();
         costXAxis.setLabel("Month");
         costYAxis.setLabel("Cost");
-        
+
         BarChart<String, Number> costChart = new BarChart<>(costXAxis, costYAxis);
         costChart.setTitle("Cost by month");
-        
+
         XYChart.Series costData = new XYChart.Series();
-        
+
         for (int i = 1; i <= 12; i++) {
             costData.getData().add(new XYChart.Data(getMonthName(i), l.costPerMonth(currentCar, i, year)));
         }
-        
+
         costChart.getData().add(costData);
         costChart.setLegendVisible(false);
         return costChart;
     }
-    
+
     private Chart kmsChart(int year) {
         CategoryAxis kmsXAxis = new CategoryAxis();
         NumberAxis kmsYAxis = new NumberAxis();
         kmsXAxis.setLabel("Month");
         kmsYAxis.setLabel("Driven kilometers");
-        
+
         BarChart<String, Number> kmsChart = new BarChart<>(kmsXAxis, kmsYAxis);
         kmsChart.setTitle("Driven kilometers");
-        
+
         XYChart.Series kmsData = new XYChart.Series();
-        
+
         for (int i = 1; i <= 12; i++) {
             kmsData.getData().add(new XYChart.Data(getMonthName(i), l.kmsInMonth(currentCar, i, year)));
         }
-        
+
         kmsChart.getData().add(kmsData);
         kmsChart.setLegendVisible(false);
         return kmsChart;
     }
-    
+
     private VBox carStats(Car car) {
         VBox statsLayout = new VBox();
         Label infoLabel = new Label("Statistics");
         infoLabel.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 18));
-        
+
         Label carNameLabel = new Label();
         carNameLabel.setText("Car: " + car.getName());
-        
+
         Label carTankLabel = new Label();
         carTankLabel.setText("Fuel tank capacity: " + car.getFuelcapacity() + " liters");
-        
+
         Label avgConsLabel = new Label();
         double avg = l.avgConsumption(car);
         DecimalFormat df = new DecimalFormat("#.##");
         avgConsLabel.setText("Average consumption: " + df.format(avg) + " l/100km");
-        
+
         Label totalVolumeLabel = new Label();
         totalVolumeLabel.setText("Fuel consumed: " + df.format(l.totalVolume(car)) + " liters");
-        
+
         Label totalCostLabel = new Label();
         totalCostLabel.setText("Total fuel cost: " + df.format(l.totalCost(car)) + " €");
-        
+
         Label totalKmsLabel = new Label();
         totalKmsLabel.setText("Total driven kilometers: " + l.totalKms(car) + " km");
-        
+
         Label numOfRefuelingsLabel = new Label();
         numOfRefuelingsLabel.setText("Refuelings: " + l.numberOfRefuelings(car));
-        
+
         statsLayout.setSpacing(5);
         statsLayout.getChildren().addAll(infoLabel, carNameLabel, carTankLabel,
                 avgConsLabel, totalVolumeLabel, totalCostLabel, totalKmsLabel,
                 numOfRefuelingsLabel);
         return statsLayout;
     }
-    
+
     public String getMonthName(int month) {
         return new DateFormatSymbols().getMonths()[month - 1];
     }
-    
+
 }
